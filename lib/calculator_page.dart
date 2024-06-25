65cf2fd628f085eefbd7aa60670d8ef3f228df2b
kabarokore <kabrokorejesco@gmail.com>
2024-06-25 18:13:15 +0200

    first commit

---
+++ lib/calculator_page.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String _expression = '';
+  String _result = '';
+
+  void _onButtonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == 'C') {
+        _expression = '';
+        _result = '';
+      } else if (buttonText == '=') {
+        try {
+          _result = _calculate(_expression);
+        } catch (e) {
+          _result = 'Error';
+        }
+      } else {
+        if (_result.isNotEmpty && !_isOperator(buttonText)) {
+          _expression = '';
+          _result = '';
+        }
+        _expression += buttonText;
+      }
+    });
+  }
+
+  bool _isOperator(String buttonText) {
+    return buttonText == '+' ||
+        buttonText == '-' ||
+        buttonText == '×' ||
+        buttonText == '÷' ||
+        buttonText == '%' ||
+        buttonText == '(' ||
+        buttonText == ')';
+  }
+
+  String _calculate(String expression) {
+    try {
+      String modifiedExpression = _handleModulus(expression);
+      modifiedExpression =
+          modifiedExpression.replaceAll('×', '*').replaceAll('÷', '/');
+      final parser = Parser();
+      final expressionAst = parser.parse(modifiedExpression);
+      final result =
+          expressionAst.evaluate(EvaluationType.REAL, ContextModel());
+      return result.toString();
+    } catch (e) {
+      return 'Error';
+    }
+  }
+
+  String _handleModulus(String expression) {
+    RegExp regex = RegExp(r'(\d+\.?\d*)\s*%\s*(\d+\.?\d*)');
+    while (regex.hasMatch(expression)) {
+      expression = expression.replaceFirstMapped(regex, (match) {
+        double leftOperand = double.parse(match.group(1)!);
+        double rightOperand = double.parse(match.group(2)!);
+        double result = leftOperand % rightOperand;
+        return result.toString();
+      });
+    }
+    return expression;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Calculator',
+          style: TextStyle(color: const Color.fromARGB(255, 10, 10, 10)),
+        ),
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        color: Color(0xFF1E1E1E),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: SingleChildScrollView(
+                    reverse: true,
+                    scrollDirection: Axis.horizontal,
+                    child: Text(
+                      _expression,
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: SingleChildScrollView(
+                    reverse: true,
+                    scrollDirection: Axis.horizontal,
+                    child: Text(
+                      _result,
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              flex: 6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButtonRow(['(', ')', 'C', '÷']),
+                  buildButtonRow(['7', '8', '9', '×']),
+                  buildButtonRow(['4', '5', '6', '-']),
+                  buildButtonRow(['1', '2', '3', '+']),
+                  buildButtonRow(['0', '.', '=', '%']),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButtonRow(List<String> buttonTexts) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: buttonTexts
+          .map((text) =>
+              Flexible(child: CalculatorButton(text, _onButtonPressed)))
+          .toList(),
+    );
+  }
+}
+
+class CalculatorButton extends StatelessWidget {
+  final String _text;
+  final Function _onPressed;
+
+  CalculatorButton(this._text, this._onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 202, 246, 100), Color.fromARGB(255, 239, 255, 100)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: ElevatedButton(
+          child: Text(
+            _text,
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          onPressed: () => _onPressed(_text),
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(20),
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
